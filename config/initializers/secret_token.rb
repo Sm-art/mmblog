bc46eaa833b85e9b5dd1e45ba1df46f1b6000003
Marta <ale_to_nic@wp.pl>
2012-07-17 19:31:52 +0200

    initiall commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '15871db765895701548f2a902317d8b9b08d9dacf68e31081880535a3111093f29956292640987be2b52284b5a9736ba7f397ad7c7a14768073dfd00b5b01bcd'
